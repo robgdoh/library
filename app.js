216ee61e5a98c9670420c6efca004901db1fbb35
dohertyr1 <dohertyr1@student.unimelb.edu.au>
2020-04-20 09:28:55 +1000

    heroku config

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(express.urlencoded({extended:true}));
 // Handle author-management requests
 // the author routes are added onto the end of '/author-management'
 app.use('/author-management', authorRouter);
-app.listen(3000, () => {
-    console.log('The library app is listening on port 3000!')
+app.listen(process.env.PORT || 3000, () => {
+    console.log("The library app is running!");
 });
