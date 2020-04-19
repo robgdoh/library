d51355300b65b6252fa40ca0490fa00a2aeb249d
dohertyr1 <dohertyr1@student.unimelb.edu.au>
2020-04-20 09:51:18 +1000

    added controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,48 +1,43 @@
+const mongoose = require("mongoose");
 
-// provide the controller a link to the author model
-const fs = require('fs');
-const authors = require('../models/author.js');
-const express = require('express');
+// import author model
+const Author = mongoose.model("author");
 
-const app = express();
+    
+// function to handle a request to get all authors
+const getAllAuthors = async (req, res) => {
+    
+  try {
+    const all_authors = await Author.find();
+    return res.send(all_authors);
+  } catch (err) {
+    res.status(400);
+    return res.send("Database query failed");
+  }
+};
+    
+  
+  
 
-// Function to handle a request to get all authors
-const getAllAuthors = (req, res) => {
-    res.send(authors); // return the list of authors
+// function to modify author by ID
+const updateAuthor = async (req, res) => {
+  res.send("Working on this feature");
 };
 
-// Function to handle a request to a particular author
+// function to add author
+const addAuthor = async (req, res) => {
+ res.send("Working on this feature");
+};
+
+// function to get author by id
 const getAuthorByID = (req, res) => {
-// search for author in the database via ID
-    const author = authors.find(author => author.id === req.params.id);
-    if (author){
-        res.send(author); // send back the author details
-    }
-    else{
-// you can decide what to return if author is not found
-// currently, an empty list will be return.
-        res.send([]);
-    }
+  res.send("Working on this feature");
 };
 
-const addAuthor = (req, res) => {
-    console.log("received post");
-    let author = req.body;
-    authors.push(author);
-    res.send(author);
-    console.log("%j", author);
-};
-
-const modifyAuthor = (req,res) => {
-    console.log("modifying");
-    let authorIx = authors.findIndex(author => author.id === req.body.id);
-    authors.splice(authorIx,1, req.body);
-    console.log(authors);
-    res.send(authors);
-
-};
-
-// Remember to export the callbacks
+// remember to export the functions
 module.exports = {
-    getAllAuthors, getAuthorByID, addAuthor, modifyAuthor
+  getAllAuthors,
+  getAuthorByID,
+  addAuthor,
+  updateAuthor
 };
